3bd5f972ce4db5fdf43b8ce35b13b616d5131141
Henry Borska <henryborska@wustl.edu>
2023-07-23 10:43:47 -0400

    Adding posts route, changing model, getting the hang of prisma and trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,10 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
+  console.log(data);
 
   return (
     <>
@@ -16,10 +17,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {!user.isSignedIn && (
-          <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
-        )}
-        {user.isSignedIn && <SignOutButton />}
+        <div>
+          {!user.isSignedIn && (
+            <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+          )}
+          {user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
