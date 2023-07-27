4054d8ec3214329d0c3301c445e82a73d0dfeb6f
Henry Borska <henryborska@wustl.edu>
2023-07-27 08:48:02 -0400

    New files for new routes - profile and post views

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -104,8 +104,12 @@ const PostView = (props: PostWithUser) => {
       />
       <div>
         <div className="flex gap-1 font-thin text-slate-300">
-          <span>{`@${author.username!}`}</span>
-          <span>{` · ${dayjs(post.createdAt).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username!}`}</span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span>{` · ${dayjs(post.createdAt).fromNow()}`}</span>
+          </Link>
         </div>
         <span className="text-2xl"> {post.content}</span>
       </div>
